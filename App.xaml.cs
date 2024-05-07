820ab9991ad343d9b6facda3f18fe2134ede89c8
Ngô Mạnh Hùng <sundayy.dev@gmail.com>
2024-05-07 21:27:58 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace media_player
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
